d32a71accd73f50390c0f075a7b3bbc2a3534583
Anderson Marques <marques_sep@hotmail.com>
2022-09-01 19:32:44 -0300

    Configuracao de banco de dados MYSQL

--- src/main/java/br/com/fiap/abctechapi/controller/HealthCheckController.java
+++ src/main/java/br/com/fiap/abctechapi/controller/HealthCheckController.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.awt.event.InputEvent;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
@@ -18,7 +17,7 @@ public class HealthCheckController {
     public ResponseEntity<String> status() throws IOException {
 
         Properties properties = new Properties();
-        InputStream inputStream = getClass().getClassLoader().getResourceAsStream("application.yaml");
+        InputStream inputStream = getClass().getClassLoader().getResourceAsStream("application.yml");
         properties.load(inputStream);
 
         return ResponseEntity.ok(properties.getProperty("build.name") + " - " + properties.getProperty("build.version"));
