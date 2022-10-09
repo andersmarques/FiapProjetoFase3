bab447bbee5e2a2106c550f6b863123bc54b2c6a
Anderson Marques <marques_sep@hotmail.com>
2022-10-09 16:12:14 -0300

    Ajuste CORS

--- src/main/java/br/com/fiap/abctechapi/WebConfig.java
+++ src/main/java/br/com/fiap/abctechapi/WebConfig.java
@@ -1,4 +1,4 @@
-package br.com.fiap.abctechservice;
+package br.com.fiap.abctechapi;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
